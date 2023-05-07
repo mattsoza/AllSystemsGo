a8dee683e260df6d7030dbc8e62e0efe2eeb4d99
PaperCIoud <haoni.amy@gmail.com>
2023-05-07 17:38:56 -0400

    add tangram shapes + puzzle logic

--- scripts/dropZone.gd
+++ scripts/dropZone.gd
@@ -1,5 +1,15 @@
-extends Marker2D
+extends Node2D
 
 
 func _draw():
-	draw_circle(Vector2.ZERO, 25, Color.BLACK)
+	draw_circle(Vector2.ZERO, 50, Color.WHITE)
+
+"""
+func select():
+	for child in get_tree().get_nodes_in_group("zone"):
+		child.deselect()
+	
+func deselect():
+"""
+
+	
