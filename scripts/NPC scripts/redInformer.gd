ae68ecd10fa2ff16eac306fd2ff6cc9ff09a40da
mattsoza <mattsoza@protonmail.com>
2023-05-09 17:57:13 -0400

    Replaced duck with red blood cell

--- scripts/NPC scripts/redInformer.gd
+++ scripts/NPC scripts/redInformer.gd
@@ -5,7 +5,7 @@ func _ready():
 
 func interact():
 	if Input.is_action_pressed("interact") and has_overlapping_bodies():	
-		var current_dialog = Dialogic.start('RedInformer1')
+		var current_dialog = Dialogic.start('redInformer1')
 		add_child(current_dialog)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
